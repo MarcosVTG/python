e2e8abfac49f39a166c0162ef3754a1d04f19e88
Marcos <mvtgomes.ege@uesc.br>
2023-11-24 10:58:32 -0300

    Implementado topicos 2,3 e 4

--- P001.py
+++ P001.py
@@ -1,6 +1,6 @@
 a = 5
 b = 3
-
+#Exercicio 2 Atividade 1  - Tópico 01
 # Operadores Aritméticos
 soma = a + b
 print(f'Adição: {soma}')
@@ -46,3 +46,36 @@ print(f'Resto da Divisão e Atribuição: {c}')
 
 c **= b
 print(f'Potenciação e Atribuição: {c}')
+
+#Tópico 02, 03 e 04
+def fatorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * fatorial(n - 1)
+
+resultado_fatorial = fatorial(30)
+print(f'O fatorial de 30 é: {resultado_fatorial}')
+
+import ctypes
+
+maior_valor_inteiro_cpp = ctypes.c_ulonglong(-1).value
+print(f'Maior valor inteiro em C++: {maior_valor_inteiro_cpp}')
+
+a = 5
+b = a + 3
+print(f'Depois da alteração: {b}')
+
+num = 12345
+
+str_num = str(num)
+print(f'Número como string: {str_num}')
+
+bin_num = bin(num)
+print(f'Representação binária: {bin_num}')
+
+hex_num = hex(num)
+print(f'Representação hexadecimal: {hex_num}')
+
+oct_num = oct(num)
+print(f'Representação octal: {oct_num}')
