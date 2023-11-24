23a6ecbdb1dda6b5be13d9db48dc6565a06da690
Marcos <mvtgomes.ege@uesc.br>
2023-11-24 11:17:08 -0300

    Implementada marcação de tarefas como realizadas

--- P002.py
+++ P002.py
@@ -58,3 +58,50 @@ lista_tarefas.adicionar_tarefa(descricao_tarefa)
 
 # Mostrar a lista de tarefas
 lista_tarefas.mostrar_tarefas()
+
+class Tarefa:
+    def __init__(self, descricao, finalizada=False):
+        self.descricao = descricao
+        self.finalizada = finalizada
+
+class ListaDeTarefas:
+    def __init__(self):
+        self.tarefas = []
+        self.id_counter = 1
+
+    def adicionar_tarefa(self, descricao):
+        if not descricao or not descricao[0].isupper():
+            print("Descrição inválida. A descrição deve começar com maiúscula.")
+            return
+        nova_tarefa = Tarefa(descricao)
+        self.tarefas.append((self.id_counter, nova_tarefa))
+        self.id_counter += 1
+        print("Tarefa registrada!!!")
+
+    def realizar_tarefa(self, tarefa_id):
+        for i, (id_tarefa, tarefa) in enumerate(self.tarefas):
+            if tarefa_id == id_tarefa and not tarefa.finalizada:
+                self.tarefas.insert(0, self.tarefas.pop(i))
+                tarefa.finalizada = True
+                print("Tarefa realizada!!!")
+                return
+        print("Tarefa não encontrada ou já realizada.")
+
+    def mostrar_tarefas(self):
+        print("Lista de Tarefas:")
+        for id_tarefa, tarefa in self.tarefas:
+            box = "[x]" if tarefa.finalizada else "[ ]"
+            print(f"{id_tarefa}. {tarefa.descricao} {box}")
+
+# Exemplo de uso
+lista_tarefas = ListaDeTarefas()
+lista_tarefas.adicionar_tarefa("Preparar a marmita")
+lista_tarefas.adicionar_tarefa("Arrumar a mochila")
+lista_tarefas.adicionar_tarefa("Fechar as janelas")
+
+lista_tarefas.mostrar_tarefas()
+
+tarefa_a_realizar = int(input("Digite o ID da tarefa a ser realizada: "))
+lista_tarefas.realizar_tarefa(tarefa_a_realizar)
+
+lista_tarefas.mostrar_tarefas()
