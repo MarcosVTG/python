6c378f3af0ec6df0c0205b69dfec916ed13ed378
Marcos <mvtgomes.ege@uesc.br>
2023-11-24 10:51:40 -0300

    Parte 02 da atividade implementada

---
+++ P001.py
@@ -0,0 +1,48 @@
+a = 5
+b = 3
+
+# Operadores Aritméticos
+soma = a + b
+print(f'Adição: {soma}')
+
+subtracao = a - b
+print(f'Subtração: {subtracao}')
+
+multiplicacao = a * b
+print(f'Multiplicação: {multiplicacao}')
+
+divisao = a / b
+print(f'Divisão: {divisao}')
+
+divisao_inteira = a // b
+print(f'Divisão Inteira: {divisao_inteira}')
+
+resto_divisao = a % b
+print(f'Resto da Divisão: {resto_divisao}')
+
+potencia = a ** b
+print(f'Potenciação: {potencia}')
+
+# Operadores Aritméticos Compostos
+c = 2
+
+c += a
+print(f'Adição e Atribuição: {c}')
+
+c -= b
+print(f'Subtração e Atribuição: {c}')
+
+c *= a
+print(f'Multiplicação e Atribuição: {c}')
+
+c /= b
+print(f'Divisão e Atribuição: {c}')
+
+c //= a
+print(f'Divisão Inteira e Atribuição: {c}')
+
+c %= b
+print(f'Resto da Divisão e Atribuição: {c}')
+
+c **= b
+print(f'Potenciação e Atribuição: {c}')
